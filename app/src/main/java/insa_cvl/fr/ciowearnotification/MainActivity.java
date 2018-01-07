e9a48be09e8ffcba7fec161df8ee58452fbb8da9
jf <jean-francois.lalande@irisa.fr>
2018-01-07 09:45:52 +0100

    Removing commented Wearable specificities

--- app/src/main/java/insa_cvl/fr/ciowearnotification/MainActivity.java
+++ app/src/main/java/insa_cvl/fr/ciowearnotification/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
                         .setContentTitle("Hey !")
                         .setContentText("You should check this information...");
 
-        /*
+
         // Wearable specificities
         // ======================
 
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Add action
         mBuilder.extend(new WearableExtender().addAction(action));
-        */
+
 
         // Fire the notification
         // =====================
